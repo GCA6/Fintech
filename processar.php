97ed5aea6ffabe9b2f757b2508e4fd91a45c700b
unknown <giovannacordeiro96@gmail.com>
2023-09-11 08:08:46 -0300

    Criação das páginas index, entrada, processar, histórico e styles.

---
+++ processar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Fintech</title>
+</head>
+<body>
+    <header>
+        <h1>Desenvolvimento Web</h1>
+        <h2>Simulação: Resultado</h2>
+    </header>
+    <main></main>
+    <footer>
+        <p>&copy;2023 - GC&AT</p>
+    </footer>
+</body>
+</html>
