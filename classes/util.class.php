79f3c624e3de8692248aff777c271548c96a05e8
unknown <giovannacordeiro96@gmail.com>
2023-09-14 09:07:28 -0300

    Alterações na classe Util e finalização das páginas entrada e index

--- classes/util.class.php
+++ classes/util.class.php
@@ -1,14 +1,14 @@
 <?php
 class Util {
-    public static function rendimento($aporte_inicial, $periodo, $rendimento_mensal, $aporte_mensal)
+    public static function rendimento($aporte_inicial, $periodo, $rendimento_m, $aporte_mensal)
     {
         $total = $aporte_inicial;
         $dados = array();
         for ($i = 1; $i <= $periodo; $i++) {
             $aporte = ($i == 1) ? 0 : $aporte_mensal;
-            $rendimento = ($total + $aporte) * ($rendimento_mensal / 100);
+            $rendimento = ($total + $aporte) * ($rendimento_m / 100);
             $total += $aporte + $rendimento;
-            $dados[] = array('mes' => $i, 'inicial' => $aporte_inicial, 'aporte' => $aporte, 'rendimento' => $rendimento, 'total' => $total);
+            $dados[] = array('mes' => $i, 'inicial' => $aporte_inicial, 'aporte' => $aporte, 'rendimento' => $rendimento, 'total' => $total, 'periodo' => $periodo);
         }
         return array('dados' => $dados, 'total' => $total);
     }
